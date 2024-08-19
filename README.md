87c1d94d01f6b3547e721d526fc4fdee4fb04ab7
Mehmet Alakuş <mehmetalakus02@icloud.com>
2024-08-20 02:44:25 +0300

    Add project files.

---
+++ README.md
@@ -0,0 +1,18 @@
+# MyTextProcessingPackage
+
+This is a test NuGet package that provides basic text processing functionalities.
+
+## Features
+
+* Convert text to uppercase (`ToUpperCase`).
+* Convert text to lowercase (`ToLowerCase`).
+* Count the number of words in a text (`CountWords`).
+
+## Installation
+
+```bash
+# Installation using NuGet Package Manager
+Install-Package MyTextProcessingPackage
+
+# Installation using dotnet CLI
+dotnet add package MyTextProcessingPackage
---
+++ TextProcessor.cs
@@ -0,0 +1,25 @@
+﻿namespace MyTextProccesingPackage;
+public class TextProcessor
+{
+    public static string ToUpperCase(string text)
+    {
+        return text.ToUpper();
+    }
+
+    public static string ToLowerCase(string text)
+    {
+        return text.ToLower();
+    }
+
+    public static int CountWords(string text)
+    {
+        return text.Split(new char[]
+        {
+            ' ',
+            '\t',
+            '\n',
+            '\r'
+        }, StringSplitOptions.RemoveEmptyEntries).Length;
+    }
+
+}
